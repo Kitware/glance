9082f8456d29f7526de7e683cd046d8fb38e6eee
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-01-24 16:08:47 -0700

    fix(InfoPolyData): add better number formatting

--- Sources/controls/Informations/PolyData.js
+++ Sources/controls/Informations/PolyData.js
@@ -5,18 +5,34 @@ import CollapsibleWidget from 'paraviewweb/src/React/Widgets/CollapsibleWidget';
 
 import style from './Informations.mcss';
 
+function formatNumbers(n) {
+  const sections = [];
+  let size = n;
+  while (size > 1000) {
+    sections.push(`000${size % 1000}`.slice(-3));
+    size = Math.floor(size / 1000);
+  }
+  if (size > 0) {
+    sections.push(size);
+  }
+  sections.reverse();
+  return sections.join("'");
+}
+
 export default function PolyData(props) {
   return (
     <CollapsibleWidget title="PolyData">
       <section className={style.section}>
         <label className={style.label}>Number of points</label>
         <span className={style.value}>
-          {props.dataset.getPoints().getNumberOfPoints()}
+          {formatNumbers(props.dataset.getPoints().getNumberOfPoints())}
         </span>
       </section>
       <section className={style.section}>
         <label className={style.label}>Number of cells</label>
-        <span className={style.value}>{props.dataset.getNumberOfCells()}</span>
+        <span className={style.value}>
+          {formatNumbers(props.dataset.getNumberOfCells())}
+        </span>
       </section>
       <section className={style.section}>
         <label className={style.label}>Bounds</label>

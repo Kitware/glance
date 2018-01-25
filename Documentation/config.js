63f0a1914e25417c9dbeddb75b754335d9086f18
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-01-25 10:34:33 -0700

    doc(website): Update web site generation

---
+++ Documentation/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  baseUrl: '/pv-web-viewer',
+  work: './build-tmp',
+  config: {
+    title: 'ParaView Glance',
+    description: '"The Web Viewer for your data"',
+    subtitle: '"Enable visualization on any computer."',
+    author: 'Kitware Inc.',
+    timezone: 'UTC',
+    url: 'https://kitware.github.io/pv-web-viewer',
+    root: '/pv-web-viewer/',
+    github: 'kitware/pv-web-viewer',
+    google_analytics: 'UA-90338862-10',
+  },
+  copy: [{ src: '../Distribution/*', dest: './build-tmp/public/app' }],
+};

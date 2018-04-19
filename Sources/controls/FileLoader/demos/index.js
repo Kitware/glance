bacdc30c2a1d09e1768dff33a711fd9a2f269110
Forrest Li <forrest.li@kitware.com>
2018-04-19 14:29:41 -0400

    WIP fix(FileLoader): Add more demos

--- Sources/controls/FileLoader/demos/index.js
+++ Sources/controls/FileLoader/demos/index.js
@@ -5,7 +5,27 @@ const Demos = [
   {
     name: 'Lysozyme.pdb',
     image: Images.Lysozyme,
-    url: 'https://data.kitware.com/api/v1/item/5aaf9eca8d777f068578dbd9/download',
+    url: 'https://kitware.github.io/vtk-js-datasets/data/pdb/2LYZ.pdb',
+  },
+  {
+    name: 'MRHead.nrrd',
+    image: Images.MRHead,
+    url: 'https://kitware.github.io/vtk-js-datasets/data/nrrd/MR-head.nrrd',
+  },
+  {
+    name: 'Tooth.nrrd',
+    image: Images.Tooth,
+    url: 'https://kitware.github.io/vtk-js-datasets/data/nrrd/tooth.nrrd',
+  },
+  {
+    name: 'Backpack.vti',
+    image: Images.Backpack,
+    url: 'https://kitware.github.io/vtk-js-datasets/data/vti/backpack.vti',
+  },
+  {
+    name: 'SinglePin.vtp',
+    image: Images.SinglePin,
+    url: 'https://kitware.github.io/vtk-js-datasets/data/vtp/single-pin.vtp',
   },
 ];
 

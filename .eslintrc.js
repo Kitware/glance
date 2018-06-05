05c7474f075ded726f0257ff9fa83bcd8304a9e7
Forrest Li <forrest.li@kitware.com>
2018-06-05 17:39:18 -0400

    chore(eslint): Use airbnb-base rules first
    Avoids conflicting rules for comma-dangle.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,9 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     extends: [
+      'airbnb-base',
       'plugin:vue/recommended',
       'plugin:prettier/recommended',
-      'airbnb-base',
     ],
     // required to lint *.vue files
     plugins: [

750fdfc9fd0ec9a65448ec7f7e168d1e91a33b3d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-06 12:02:32 -0600

    feat(VtkView): Add getView function

--- src/components/core/VtkView/script.js
+++ src/components/core/VtkView/script.js
@@ -1,9 +1,39 @@
 let subscriptions = [];
 
+// ----------------------------------------------------------------------------
+// Global methods
+// ----------------------------------------------------------------------------
+
+function getView(proxyManager, type, name) {
+  let view = null;
+  const views = proxyManager.getViews();
+  for (let i = 0; i < views.length; i++) {
+    if (views[i].getProxyName() === type) {
+      if (name) {
+        if (views[i].getReferenceByName('name') == name) {
+          view = views[i];
+        }
+      } else {
+        view = views[i];
+      }
+    }
+  }
+
+  if (!view) {
+    view = proxyManager.createProxy('Views', 'View3D', { name });
+  }
+
+  return view;
+}
+
+// ----------------------------------------------------------------------------
+// Component method
+// ----------------------------------------------------------------------------
+
 function onMounted() {
   const container = this.$el;
 
-  this.view = this.proxyManager.createProxy('Views', 'View3D');
+  this.view = getView(this.proxyManager, 'View3D');
   this.view.setContainer(container);
   this.view.resetCamera();
   this.view.resize();
@@ -18,6 +48,7 @@ function onMounted() {
 }
 
 function onBeforeDestroy() {
+  this.view.setContainer(null);
   while (subscriptions.length) {
     subscriptions.pop().unsubscribe();
   }

e93b559f59790fc82f4b025d3d0870b9f9237645
Forrest Li <forrest.li@kitware.com>
2018-06-06 00:12:28 -0400

    feat(App): Initial App layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,71 @@
 <template>
-  <v-app />
+  <v-app>
+    <v-navigation-drawer
+      :value="sidebar"
+      absolute
+      app
+      clipped
+      hide-overlay
+      stateless
+    >
+      <v-tabs
+        v-model="activeTab"
+        color="grey lighten-4"
+        slider-color="green"
+      >
+        <v-tab>Datasets</v-tab>
+        <v-tab>Tools</v-tab>
+        <v-tab>Settings</v-tab>
+        <v-tab-item>Datasets</v-tab-item>
+        <v-tab-item>Tools...</v-tab-item>
+        <v-tab-item>settings...</v-tab-item>
+      </v-tabs>
+    </v-navigation-drawer>
+    <v-toolbar
+      clipped-left
+      app
+    >
+      <v-toolbar-side-icon @click.native.stop="sidebar = !sidebar" />
+      <v-toolbar-title>ParaView Glance</v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        flat
+        class="toolbar-button"
+      >
+        <v-icon>folder</v-icon>
+        <span>Open</span>
+      </v-btn>
+      <v-btn
+        flat
+        class="toolbar-button"
+      >
+        <v-icon>collections</v-icon>
+        <span>Screenshots (0)</span>
+      </v-btn>
+      <v-btn
+        flat
+        class="toolbar-button"
+      >
+        <v-icon>help</v-icon>
+        <span>About</span>
+      </v-btn>
+    </v-toolbar>
+  </v-app>
 </template>
 
+<style scoped>
+.toolbar-button span {
+  margin-left: 4px;
+}
+</style>
+
 <script>
+const data = () => ({
+  sidebar: true,
+  activeTab: 0,
+});
+
 export default {
-  name: 'App',
+  data,
 };
 </script>

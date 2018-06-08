b1a02f0b8ead986489fc7d2c5d6ae1f9905a40ff
Forrest Li <forrest.li@kitware.com>
2018-06-08 17:27:42 -0400

    chore(webpack): Add production build config
    Custom uglifyjs config to avoid uglifyjs bugs related to inline
    functions.

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,31 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.base.config');
+
+const HOST = process.env.HOST;
+const PORT = process.env.PORT && Number(process.env.PORT);
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            // uglifyjs bug: https://github.com/webpack/webpack/issues/6567
+            inline: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'static/index.html',
+      inject: 'head',
+    }),
+  ],
+});

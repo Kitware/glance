9513dabe45991eb8530dc2e42d5b8b41900aee7a
Forrest Li <forrest.li@kitware.com>
2018-06-12 13:14:29 -0400

    refactor(palette): Move generic palette to top level

--- src/components/core/VtkView/palette.js
+++ src/components/core/VtkView/palette.js
@@ -1,17 +1,10 @@
+import { SPECTRAL } from 'paraview-glance/src/palette';
+
+// Specifications for vtkview background colors, images, and gradients
 export const BACKGROUND = [
   '#000000',
   '#ffffff',
-  '#9e0142',
-  '#d53e4f',
-  '#f46d43',
-  '#fdae61',
-  '#fee08b',
-  '#ffffbf',
-  '#e6f598',
-  '#abdda4',
-  '#66c2a5',
-  '#3288bd',
-  '#5e4fa2',
+  ...SPECTRAL,
   'linear-gradient(#7478BE, #C1C3CA)', // from 3D Slicer default
 ];
 

a67fdbe29a323cf02f9e9e49c0dc0c1d4650bf76
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-20 17:56:17 -0600

    doc(sample): First step for new sample data + images

--- src/samples/images/index.js
+++ src/samples/images/index.js
@@ -1,13 +1,15 @@
 import Backpack from './backpack.jpg';
+import LIDC2 from './LIDC2.jpg';
+import Lidar from './Lidar.jpg';
 import Lysozyme from './lysozyme.jpg';
-import MRHead from './mrhead.jpg';
 import SinglePin from './singlepin.jpg';
 import Tooth from './tooth.jpg';
 
 export default {
   Backpack,
+  LIDC2,
+  Lidar,
   Lysozyme,
-  MRHead,
   SinglePin,
   Tooth,
 };

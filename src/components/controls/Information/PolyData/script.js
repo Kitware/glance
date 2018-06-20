006a24444ff24343eb295ce934dd58d38a6989be
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-20 16:36:31 -0600

    fix(Information): Add number thousand separator

--- src/components/controls/Information/PolyData/script.js
+++ src/components/controls/Information/PolyData/script.js
@@ -1,4 +1,11 @@
+import macro from 'vtk.js/Sources/macro';
+
+const { formatNumbersWithThousandSeparator } = macro;
+
 export default {
   name: 'PolyData',
   props: ['dataset'],
+  methods: {
+    formatNumbersWithThousandSeparator,
+  },
 };

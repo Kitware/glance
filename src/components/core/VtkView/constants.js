4286ce4811b5305f67953cd90dfdf20bf28ad396
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-21 15:03:29 -0600

    fix(VtkView): Orientation and layout

--- src/components/core/VtkView/constants.js
+++ src/components/core/VtkView/constants.js
@@ -2,8 +2,8 @@ export const DEFAULT_VIEW_TYPE = 'View3D:default';
 
 export const VIEW_TYPES = [
   { text: 'View 3D', value: 'View3D:default' },
-  { text: 'Orientation X', value: 'View2D_X:x' },
   { text: 'Orientation Y', value: 'View2D_Y:y' },
+  { text: 'Orientation X', value: 'View2D_X:x' },
   { text: 'Orientation Z', value: 'View2D_Z:z' },
 ];
 
@@ -16,16 +16,16 @@ export const VIEW_ORIENTATIONS = {
   x: {
     axis: 0,
     orientation: 1,
-    viewUp: [0, -1, 0],
+    viewUp: [0, 0, 1],
   },
   y: {
     axis: 1,
-    orientation: 1,
+    orientation: -1,
     viewUp: [0, 0, 1],
   },
   z: {
     axis: 2,
-    orientation: 1,
+    orientation: -1,
     viewUp: [0, -1, 0],
   },
 };

da069c4e322525ed5bec9fa3a97b516852895cd7
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-21 15:42:18 -0600

    fix(BrowserIssues): GPU feedback

--- src/components/core/BrowserIssues/script.js
+++ src/components/core/BrowserIssues/script.js
@@ -1,11 +1,39 @@
+import viewHelper from 'paraview-glance/src/components/core/VtkView/helper';
+import { DEFAULT_VIEW_TYPE } from 'paraview-glance/src/components/core/VtkView/constants';
 // ----------------------------------------------------------------------------
 // Component API
 // ----------------------------------------------------------------------------
 
 function getBrowserIssues() {
-  const webglVersion = 3333;
-  if (webglVersion === 1) {
-    this.issues.push('WebGL version is 1.0, expected at least 2.0');
+  const view = viewHelper.getView(this.proxyManager, DEFAULT_VIEW_TYPE);
+  if (view) {
+    const glRW = view.getOpenglRenderWindow();
+    const allInfo = glRW.getGLInformations();
+    const { UNMASKED_RENDERER, UNMASKED_VENDOR, WEBGL_VERSION } = allInfo;
+
+    if (WEBGL_VERSION.value < 2) {
+      this.issues.push(
+        `Detected WebGL version ${
+          WEBGL_VERSION.value
+        }. (Slower Volume/Glyph rendering)`
+      );
+    }
+
+    const strToTest = `${UNMASKED_VENDOR.value} / ${
+      UNMASKED_RENDERER.value
+    }`.toLowerCase();
+    if (strToTest.indexOf('intel') !== -1) {
+      this.issues.push(
+        `Detected GPU "${
+          UNMASKED_RENDERER.value
+        }". Dedicated GPU would offer better performances.`
+      );
+    }
+    if (strToTest.indexOf('angle') !== -1) {
+      this.issues.push(
+        `ANGLE is used to translate OpenGL for your hardware. (Slower rendering)`
+      );
+    }
   }
 
   if (this.issues.length) {
@@ -17,6 +45,7 @@ function getBrowserIssues() {
 
 export default {
   name: 'BrowserIssues',
+  inject: ['proxyManager'],
   data() {
     return {
       issues: [],

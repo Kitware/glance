1754af06c09e141388ea7507a2ea4f5136442efc
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-21 15:03:49 -0600

    fix(Sample): Update sample data list

--- src/samples/images/index.js
+++ src/samples/images/index.js
@@ -1,28 +1,24 @@
+// import Backpack from './backpack.jpg';
+// import Foot from './foot.jpg';
+// import Head from './head.jpg';
+import Aneurism from './aneurism.jpg';
+import CAD from './cad.jpg';
+import Engine from './engine.jpg';
+import Lidar from './Lidar.jpg';
 import LIDC2 from './LIDC2.jpg';
 import Lysozyme from './lysozyme.jpg';
-import Tooth from './tooth.jpg';
-
-import Aneurism from './aneurism.jpg';
-import Backpack from './backpack.jpg';
-import CAD from './cad.jpg';
-import Lidar from './Lidar.jpg';
 import SinglePin from './singlepin.jpg';
-import Engine from './engine.jpg';
-import Foot from './foot.jpg';
-import Head from './head.jpg';
 import Skull from './skull.jpg';
+import Tooth from './tooth.jpg';
 
 export default {
   Aneurism,
-  Backpack,
   CAD,
   Engine,
-  Foot,
-  Skull,
-  Head,
   Lidar,
   LIDC2,
   Lysozyme,
   SinglePin,
+  Skull,
   Tooth,
 };

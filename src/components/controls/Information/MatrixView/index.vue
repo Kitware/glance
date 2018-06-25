3fb97d51519403a9734c2ab15669c58dc1787a7b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-06-25 12:28:28 -0600

    fix(MatrixView): include style

--- src/components/controls/Information/MatrixView/index.vue
+++ src/components/controls/Information/MatrixView/index.vue
@@ -1,3 +1,3 @@
 <template src="paraview-glance/src/components/controls/Information/MatrixView/template.html" />
-<style />
+<style module src="paraview-glance/src/components/core/Datasets/style.css" />
 <script src="paraview-glance/src/components/controls/Information/MatrixView/script.js" />

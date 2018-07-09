32cb91e0b93339cc0d4eb7a2e8effdae0912c0b4
Forrest Li <forrest.li@kitware.com>
2018-07-09 12:44:22 -0400

    fix(constants): Remove unused constants

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,7 @@
-// global events
-export const Events = {
-  MSG_INFO: 'MSG_INFO',
-  MSG_ERROR: 'MSG_ERROR',
-};
-
-export const Messages = {
-  OPEN_ERROR: 'Specified file is currently unsupported',
-  DOWNLOAD_FAILED:
-    'Failed to download all specified datasets (bad URLs or adblock is on)',
-};
-
 export const Widgets = {
   CROP: 'CROP',
 };
 
 export default {
-  Events,
-  Messages,
   Widgets,
 };

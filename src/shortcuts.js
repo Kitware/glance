ce8a1ec6e586456969a96bddadfba41fd78ef90a
Forrest Li <forrest.li@kitware.com>
2018-07-17 11:51:58 -0400

    fix(shortcuts): Add screenshot shortcut

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -1,1 +1,4 @@
-export default [{ key: 'ctrl+o', action: 'PROMPT_FOR_FILES' }];
+export default [
+  { key: 'ctrl+o', action: 'PROMPT_FOR_FILES' },
+  { key: 'ctrl+g', action: 'TAKE_SCREENSHOT' },
+];

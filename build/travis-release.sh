4ca333ed5192e365c69f2b0e1ab3cafbff030b42
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-07-18 16:32:48 -0600

    ci(travis): try to fix build process for nightly

---
+++ build/travis-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+npm run build:release
+npm run validate
+npm run test:travis
+git config --global user.name "Travis CI"
+git config --global user.email "sebastien.jourdain@kitware.com"
+export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/paraview-glance.git
+npm run semantic-release
+npm run doc:publish

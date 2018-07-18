5eafb5feb7e600ef379e44afe14f4e9e1b38e5f0
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-07-18 16:39:41 -0600

    ci(travis): fix typo

--- build/travis-release.sh
+++ build/travis-release.sh
@@ -3,7 +3,6 @@ set -ev
 
 npm run build:release
 npm run validate
-npm run test:travis
 git config --global user.name "Travis CI"
 git config --global user.email "sebastien.jourdain@kitware.com"
 export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/paraview-glance.git

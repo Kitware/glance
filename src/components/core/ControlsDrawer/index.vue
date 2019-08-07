bb6735b1e1e7deb38f237886a46fc275aabf96bc
Forrest Li <forrest.li@kitware.com>
2019-08-07 13:36:57 -0400

    fix(ControlsDrawer): Handle tab content overflow

--- src/components/core/ControlsDrawer/index.vue
+++ src/components/core/ControlsDrawer/index.vue
@@ -1,2 +1,3 @@
 <template src="paraview-glance/src/components/core/ControlsDrawer/template.html" />
 <script src="paraview-glance/src/components/core/ControlsDrawer/script.js" />
+<style module src="paraview-glance/src/components/core/ControlsDrawer/style.css" />

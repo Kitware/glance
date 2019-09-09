f94a89b70a557e0ae0f6c7378b9280c750651003
Forrest Li <forrest.li@kitware.com>
2019-09-09 11:30:10 -0400

    fix(PopUp): Close on Esc and move popup to top heuristically

--- src/components/widgets/PopUp/script.js
+++ src/components/widgets/PopUp/script.js
@@ -16,9 +16,11 @@ export default {
   },
   mounted() {
     document.addEventListener('mousedown', this.handleMouse, true);
+    document.addEventListener('keydown', this.handleEsc);
   },
   beforeDestroy() {
     document.removeEventListener('mousedown', this.handleMouse, true);
+    document.removeEventListener('keydown', this.handleEsc);
   },
   methods: {
     handleMouse(ev) {
@@ -32,9 +34,21 @@ export default {
         }
       }
     },
+    handleEsc(ev) {
+      if (this.visible && ev.keyCode === 27) { // Esc
+        this.close();
+      }
+    },
     getPositioning() {
       const anchor = this.$refs.anchor;
       const { top: y, left: x } = anchor.getBoundingClientRect();
+      const viewportHeight = window.innerHeight;
+      if (y > viewportHeight / 2) {
+        return {
+          bottom: `${viewportHeight -y}px`,
+          left: `${x}px`,
+        };
+      }
       return {
         top: `${y}px`,
         left: `${x}px`,

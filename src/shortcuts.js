2ab860925b4fb309da5f1cce9e290ced3b5722e6
Forrest Li <forrest.li@kitware.com>
2019-09-11 11:34:40 -0400

    feat(App): Use f/b to navigate slices in active view

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -2,4 +2,6 @@ export default [
   { key: 'ctrl+o', action: 'PROMPT_FOR_FILES' },
   { key: 'ctrl+g', action: 'TAKE_SCREENSHOT' },
   { key: 'r', action: 'RESET_ACTIVE_CAMERA' },
+  { key: 'f', action: 'INCREASE_SLICE' },
+  { key: 'b', action: 'DECREASE_SLICE' },
 ];

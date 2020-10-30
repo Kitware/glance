a82aa26272fabdb188081df708ea9b1a14fe5979
Forrest Li <forrest.li@kitware.com>
2020-10-30 15:30:37 -0400

    feat(Distance2DWidget): Base on vtk.js widget
    Also fixes an issue where point placement is not updated when
    manipulator is updated.

--- src/vtk/Distance2DWidget/behavior.js
+++ src/vtk/Distance2DWidget/behavior.js
@@ -1,185 +1,31 @@
-import macro from 'vtk.js/Sources/macro';
-
-const MAX_POINTS = 2;
+import superWidgetBehavior from 'vtk.js/Sources/Widgets/Widgets3D/DistanceWidget/behavior';
 
 export default function widgetBehavior(publicAPI, model) {
-  model.classHierarchy.push('vtkDistanceWidgetProp');
-  let isDragging = null;
-  let cameraSub = null;
+  superWidgetBehavior(publicAPI, model);
+  const superBehavior = { ...publicAPI };
 
-  // --------------------------------------------------------------------------
-  // Display 2D
-  // --------------------------------------------------------------------------
+  const watchers = [];
+  let prevCallData = null;
 
-  publicAPI.setDisplayCallback = (callback) =>
-    model.representations[0].setDisplayCallback(callback);
-
-  // --------------------------------------------------------------------------
-  // Interactor events
-  // --------------------------------------------------------------------------
-
-  function ignoreKey(e) {
-    return e.altKey || e.controlKey || e.shiftKey;
+  function retriggerMouseMove() {
+    if (prevCallData) {
+      superBehavior.handleMouseMove(prevCallData);
+    }
   }
 
-  // --------------------------------------------------------------------------
-  // Left press: Select handle to drag
-  // --------------------------------------------------------------------------
-
-  publicAPI.handleLeftButtonPress = (e) => {
-    if (
-      !model.activeState ||
-      !model.activeState.getActive() ||
-      !model.pickable ||
-      ignoreKey(e)
-    ) {
-      return macro.VOID;
-    }
-
-    if (
-      model.activeState === model.widgetState.getMoveHandle() &&
-      model.widgetState.getHandleList().length < MAX_POINTS
-    ) {
-      // Commit handle to location
-      const moveHandle = model.widgetState.getMoveHandle();
-      const newHandle = model.widgetState.addHandle();
-      newHandle.setOrigin(...moveHandle.getOrigin());
-      newHandle.setColor(moveHandle.getColor());
-      newHandle.setScale1(moveHandle.getScale1());
-    } else {
-      isDragging = true;
-      model.openGLRenderWindow.setCursor('grabbing');
-      model.interactor.requestAnimation(publicAPI);
-    }
-
-    publicAPI.invokeStartInteractionEvent();
-    return macro.EVENT_ABORT;
-  };
-
-  // --------------------------------------------------------------------------
-  // Mouse move: Drag selected handle / Handle follow the mouse
-  // --------------------------------------------------------------------------
-
   publicAPI.handleMouseMove = (callData) => {
-    if (
-      model.hasFocus &&
-      model.widgetState.getHandleList().length === MAX_POINTS
-    ) {
-      model.widgetManager.releaseFocus();
-      return macro.VOID;
-    }
-
-    if (
-      model.pickable &&
-      model.manipulator &&
-      model.activeState &&
-      model.activeState.getActive() &&
-      !ignoreKey(callData)
-    ) {
-      const worldCoords = model.manipulator.handleEvent(
-        callData,
-        model.openGLRenderWindow
-      );
-      if (
-        worldCoords.length &&
-        (model.activeState === model.widgetState.getMoveHandle() || isDragging)
-      ) {
-        model.activeState.setOrigin(worldCoords);
-        publicAPI.invokeInteractionEvent();
-        return macro.EVENT_ABORT;
-      }
-    }
-
-    return macro.VOID;
+    const ret = superBehavior.handleMouseMove(callData);
+    prevCallData = callData;
+    return ret;
   };
 
-  // --------------------------------------------------------------------------
-  // Left release: Finish drag / Create new handle
-  // --------------------------------------------------------------------------
-
-  publicAPI.handleLeftButtonRelease = () => {
-    if (isDragging && model.pickable) {
-      model.openGLRenderWindow.setCursor('pointer');
-      model.widgetState.deactivate();
-      model.interactor.cancelAnimation(publicAPI);
-      publicAPI.invokeEndInteractionEvent();
-    } else if (model.activeState !== model.widgetState.getMoveHandle()) {
-      model.widgetState.deactivate();
-    }
-
-    if (
-      (model.hasFocus && !model.activeState) ||
-      (model.activeState && !model.activeState.getActive())
-    ) {
-      publicAPI.invokeEndInteractionEvent();
-    }
-
-    isDragging = false;
-  };
-
-  // --------------------------------------------------------------------------
-  // Focus API - modeHandle follow mouse when widget has focus
-  // --------------------------------------------------------------------------
-
-  publicAPI.grabFocus = () => {
-    if (
-      !model.hasFocus &&
-      model.widgetState.getHandleList().length < MAX_POINTS
-    ) {
-      model.activeState = model.widgetState.getMoveHandle();
-      model.activeState.activate();
-      model.activeState.setVisible(true);
-      model.interactor.requestAnimation(publicAPI);
-      publicAPI.invokeStartInteractionEvent();
-    }
-    model.hasFocus = true;
-  };
-
-  // --------------------------------------------------------------------------
-
-  publicAPI.loseFocus = () => {
-    if (model.hasFocus) {
-      model.interactor.cancelAnimation(publicAPI);
-      publicAPI.invokeEndInteractionEvent();
-    }
-    model.widgetState.deactivate();
-    model.widgetState.getMoveHandle().deactivate();
-    model.widgetState.getMoveHandle().setVisible(false);
-    model.activeState = null;
-    model.hasFocus = false;
-    model.widgetManager.enablePicking();
-    model.interactor.render();
-  };
-
-  // --------------------------------------------------------------------------
-
-  publicAPI.delete = macro.chain(publicAPI.delete, () => {
-    if (cameraSub) {
-      cameraSub.unsubscribe();
-    }
-  });
-
-  // --------------------------------------------------------------------------
-  // init
-  // --------------------------------------------------------------------------
-
-  const setHandleScaleFromCamera = () => {
-    if (model.camera) {
-      let scale;
-      if (model.camera.getParallelProjection()) {
-        scale = model.camera.getParallelScale() / 1.25;
-      } else {
-        scale = model.camera.getDistance() / 6;
-      }
-
-      publicAPI.setHandleScale(scale);
+  publicAPI.delete = () => {
+    superBehavior.delete();
+    while (watchers.length) {
+      watchers.pop().unsubscribe();
     }
   };
 
-  // listen to camera so we can scale the handles to the screen
-  cameraSub = model.camera.onModified(setHandleScaleFromCamera);
-  // since forwarded/linked set/get methods aren't created until
-  // after this behavior function finishes, this is a hack to invoke
-  // initial handle scale.
-  setTimeout(setHandleScaleFromCamera, 0);
+  // re-place the point when manipulator changes
+  watchers.push(model.manipulator.onModified(() => retriggerMouseMove()));
 }
--- src/vtk/Distance2DWidget/index.js
+++ src/vtk/Distance2DWidget/index.js
@@ -1,12 +1,10 @@
 import macro from 'vtk.js/Sources/macro';
-import vtkAbstractWidgetFactory from 'vtk.js/Sources/Widgets/Core/AbstractWidgetFactory';
-import vtkPlanePointManipulator from 'vtk.js/Sources/Widgets/Manipulators/PlaneManipulator';
-import { distance2BetweenPoints } from 'vtk.js/Sources/Common/Core/Math';
+import vtkSphereHandleRepresentation from 'vtk.js/Sources/Widgets/Representations/SphereHandleRepresentation';
+import vtkDistanceWidget from 'vtk.js/Sources/Widgets/Widgets3D/DistanceWidget';
 
 import widgetBehavior from 'paraview-glance/src/vtk/Distance2DWidget/behavior';
 import stateGenerator from 'paraview-glance/src/vtk/Distance2DWidget/state';
 
-import vtkScaledSphereHandleRepresentation from 'paraview-glance/src/vtk/ScaledSphereHandleRepresentation';
 import vtkSVGCircleHandleRepresentation from 'paraview-glance/src/vtk/SVGCircleHandleRepresentation';
 import vtkSVGLineRepresentation from 'paraview-glance/src/vtk/SVGLineRepresentation';
 import vtkSVGLabelRepresentation from 'paraview-glance/src/vtk/SVGLabelRepresentation';
@@ -17,22 +15,18 @@ import { ViewTypes } from 'vtk.js/Sources/Widgets/Core/WidgetManager/Constants';
 // Factory
 // ----------------------------------------------------------------------------
 
-function vtkDistanceWidget(publicAPI, model) {
-  model.classHierarchy.push('vtkDistanceWidget');
+function vtkDistance2DWidget(publicAPI, model) {
+  model.classHierarchy.push('vtkDistance2DWidget');
 
   // --- Widget Requirement ---------------------------------------------------
 
   model.methodsToLink = [
-    'activeScaleFactor',
-    'activeColor',
-    'useActiveColor',
-    'defaultScale',
+    ...(model.methodsToLink ?? []),
     'circleProps',
     'lineProps',
     'textProps',
     'text',
     'textStateIndex',
-    'handleScale',
   ];
   model.behavior = widgetBehavior;
   model.widgetState = stateGenerator();
@@ -45,10 +39,19 @@ function vtkDistanceWidget(publicAPI, model) {
       case ViewTypes.VOLUME:
       default:
         return [
-          { builder: vtkScaledSphereHandleRepresentation, labels: ['handles'] },
           {
-            builder: vtkScaledSphereHandleRepresentation,
+            builder: vtkSphereHandleRepresentation,
+            labels: ['handles'],
+            initialValues: {
+              scaleInPixels: true,
+            },
+          },
+          {
+            builder: vtkSphereHandleRepresentation,
             labels: ['moveHandle'],
+            initialValues: {
+              scaleInPixels: true,
+            },
           },
           {
             builder: vtkSVGCircleHandleRepresentation,
@@ -65,56 +68,25 @@ function vtkDistanceWidget(publicAPI, model) {
         ];
     }
   };
-
-  // --- Public methods -------------------------------------------------------
-
-  publicAPI.getDistance = () => {
-    const handles = model.widgetState.getHandleList();
-    if (handles.length !== 2) {
-      return 0;
-    }
-    return Math.sqrt(
-      distance2BetweenPoints(handles[0].getOrigin(), handles[1].getOrigin())
-    );
-  };
-
-  // --------------------------------------------------------------------------
-  // initialization
-  // --------------------------------------------------------------------------
-
-  model.widgetState.onBoundsChange((bounds) => {
-    const center = [
-      (bounds[0] + bounds[1]) * 0.5,
-      (bounds[2] + bounds[3]) * 0.5,
-      (bounds[4] + bounds[5]) * 0.5,
-    ];
-    model.widgetState.getMoveHandle().setOrigin(center);
-  });
-
-  // Default manipulator
-  model.manipulator = vtkPlanePointManipulator.newInstance();
 }
 
 // ----------------------------------------------------------------------------
 
-const DEFAULT_VALUES = {
-  // manipulator: null,
-};
+const DEFAULT_VALUES = {};
 
 // ----------------------------------------------------------------------------
 
 export function extend(publicAPI, model, initialValues = {}) {
   Object.assign(model, DEFAULT_VALUES, initialValues);
 
-  vtkAbstractWidgetFactory.extend(publicAPI, model, initialValues);
-  macro.setGet(publicAPI, model, ['manipulator']);
+  vtkDistanceWidget.extend(publicAPI, model, initialValues);
 
-  vtkDistanceWidget(publicAPI, model);
+  vtkDistance2DWidget(publicAPI, model);
 }
 
 // ----------------------------------------------------------------------------
 
-export const newInstance = macro.newInstance(extend, 'vtkDistanceWidget');
+export const newInstance = macro.newInstance(extend, 'vtkDistance2DWidget');
 
 // ----------------------------------------------------------------------------
 
